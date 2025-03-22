807bf60ebd1615da56f6923e1f7271637b48d589
Ahmed Shabab Noor <57612672+ashababnoor@users.noreply.github.com>
2025-03-22 17:42:50 +0600

    feat: add example Go file to demonstrate calling C functions

---
+++ cgotest/example1.go
@@ -0,0 +1,16 @@
+package cgotest
+
+/*
+#include <stdio.h>
+
+// Define a simple C function
+void sayHello() {
+    printf("Hello from C!\n");
+}
+*/
+import "C"
+
+func Example1() {
+	// Call the C function from Go
+	C.sayHello()
+}

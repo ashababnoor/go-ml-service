4b1926a2667a4cab5408f0f39aa692e970510ca8
Ahmed Shabab Noor <57612672+ashababnoor@users.noreply.github.com>
2025-04-06 17:21:00 +0600

    feat: add CGoExamples function to demonstrate calling multiple C functions from Go

---
+++ cgoexamples.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	example "github.com/ashababnoor/go-ml-service/cgoexample"
+)
+
+func CGoExamples() {
+	fmt.Println("Hello from Go!")
+
+	example.Example1()
+	example.Example2()
+	example.Example3()
+	example.Example4()
+}
